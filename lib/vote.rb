2867e9ca6799c7a81ca3b1d4f23831a0d6b91e31
Robert Woolley <rob@robertwoolley.co.uk>
2020-02-27 12:46:40 +0000

    Voting works now on cat or not

--- lib/vote.rb
+++ lib/vote.rb
@@ -6,8 +6,6 @@ class Vote
     vote_score = 0
     vote_score = 1 if votingcatornot == 'cat'
     hash = { image_id: imageid, value: vote_score }
-    p hash
     @json = hash.to_json
-    p @json
   end
 end
--- spec/vote_spec.rb
+++ spec/vote_spec.rb
@@ -7,4 +7,9 @@ describe Vote do
     parsed_body = JSON.parse(test_vote.json)
     expect(parsed_body['image_id']).to eq('kitty1234')
   end
+  it 'creates an expected JSON voting object using voting intention' do
+    test_vote = Vote.new('cat', 'kitty1234')
+    parsed_body = JSON.parse(test_vote.json)
+    expect(parsed_body['value']).to eq(1)
+  end
 end
